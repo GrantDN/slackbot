2b7b59d99c4e566d1da99105b0a883075f5334d3
Henrik Ilisson <hllisson@MBP-HILISSON.local>
2019-04-01 10:06:34 +0300

    slight edit 10:06

--- main.js
+++ main.js
@@ -14,3 +14,11 @@ app.use(bodyParser.json());
 const listener = app.listen(process.env.PORT || '3000', function () {
   console.log('Your app is listening on port ' + listener.address().port);
 })
+
+app.post('/action-endpoint', function (req, res) {
+  const challenge = req.body.challenge;
+  const reply = {
+    "challenge": challenge
+};
+res.json(reply);
+});
