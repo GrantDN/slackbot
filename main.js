cbc18fa64e96e9febaee7a883858d57fa500c759
Henrik Ilisson <hllisson@MBP-HILISSON.local>
2019-04-01 11:22:23 +0300

    change 11:22

--- main.js
+++ main.js
@@ -11,40 +11,16 @@ const request = require('request');
 
 app.use(bodyParser.json());
 
-const listener = app.listen(process.env.PORT || '3000', function () {
+const listener = app.listen(process.env.PORT || '3000', function ()
+{
   console.log('Your app is listening on port ' + listener.address().port);
 })
 
-app.post('/action-endpoint', function (req, res) {
+app.post('/action-endpoint', function (req, res)
+{
   const challenge = req.body.challenge;
   const reply = {
     "challenge": challenge
-};
-res.json(reply);
-});
-  const headers = {
-    'Content-type': 'application/json',
-    'Authorization': 'Bearer ${process.env.TOKEN}'
-  }
-
-  if (req.body.event.subtype != 'bot_message') {
-    const body = {
-      'channel': req.body.event.channel,
-      'text': req.body.event.text
-    }
-
-    const options = {
-      url: 'https://slack.com/api/chat.postMessage',
-      method: 'POST',
-      headers,
-      body: JSON.stringify(body)
-    };
-
-    request.post(options, function(err, res, body) {
-      if (err) {
-        console.log(err);
-      }
-    })
-  }
-res.json(reply);
-});
+  };
+  res.json(reply);
+}       );
