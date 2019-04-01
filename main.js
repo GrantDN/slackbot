80657ed5aaa025a9d46ab86d8454d4f3ccbe8eb9
Henrik Ilisson <hllisson@MBP-HILISSON.local>
2019-04-01 11:54:51 +0300

    changes 11:54 Pray4Me

--- main.js
+++ main.js
@@ -29,23 +29,34 @@ app.post('/action-endpoint', function (req, res)
   }
 
   if (req.body.event.subtype != 'bot_message') {
-    const body = {
-      'channel': req.body.event.channel,
-      'text': req.body.event.text
-    }
-
-    const options = {
-      url: 'https://slack.com/api/chat.postMessage',
-      method: 'POST',
-      headers,
-      body: JSON.stringify(body)
-    };
-
-    request.post(options, function(err, res, body) {
+    request.get('https://api.coindesk.com/v1/bpi/currentprice/EUR.json', function(err, res, body) {
       if (err) {
         console.log(err);
       }
-    })
+      else {
+        const coindesk = JSON.parse(body);
+        const rate = coindesk.bpi.EUR.rate;
+        const reply = {
+          'channel': req.body.event.channel,
+          text: `Current BTC rate: ${rate} EUR per 1 BTC`
+        }
+
+        const options = {
+          url: 'https://slack.com/api/chat.postMessage',
+          method: 'POST',
+          headers,
+          body: JSON.stringift(reply)
+        };
+
+        console.log(body);
+
+        request.post(options, function(err, res, body) {
+          if (err) {
+            console.log(err);
+          }
+        });
+      }
+    });
   }
   res.json(reply);
 }       );
