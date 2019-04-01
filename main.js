e5a84c799453bbe430902c5a3d7a99b8d876e6f4
Henrik Ilisson <hllisson@MBP-HILISSON.local>
2019-04-01 10:15:56 +0300

    changes 10:14

--- main.js
+++ main.js
@@ -20,5 +20,30 @@ app.post('/action-endpoint', function (req, res) {
   const reply = {
     "challenge": challenge
 };
+
+  const headers = {
+    'Content-type': 'application/json',
+    'Authorization': 'Bearer ${process.env.TOKEN}'
+  }
+
+  if (req.body.event.subtype != 'bot_message') {
+    const body = {
+      'channel': req.body.event.channel,
+      'text': req.body.event.text
+    }
+
+    const options = {
+      url: 'https://slack.com/api/chat.postMessage',
+      method: 'POST',
+      headers,
+      body: JSON.stringify(body)
+    };
+
+    request.post(options, function(err, res, body) {
+      if (err) {
+        console.log(err);
+      }
+    })
+  }
 res.json(reply);
 });
